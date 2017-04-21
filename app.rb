3e3cf3066cc8d1bc68a100de8b7f4223e1902e26
Spencer Alan <spencer.alan@icloud.com>
2017-04-21 16:17:16 -0700

    refactor app.rb logic

--- app.rb
+++ app.rb
@@ -19,48 +19,48 @@ get('/phrase_parser') do
 
   if phrases.is_word?(phrase1) && phrases.is_word?(phrase2)
 
-    if phrases.is_palindrome?(phrase1)
-      @phrase1_palindrome_message = "'#{phrase1}' is a palindrome"
+    @phrase1_palindrome_message = if phrases.is_palindrome?(phrase1)
+     "'#{phrase1}' is a palindrome"
     else
-      @phrase1_palindrome_message = "'#{phrase1}' is not a palindrome"
+      "'#{phrase1}' is not a palindrome"
     end
 
-    if phrases.is_palindrome?(phrase2)
-      @phrase2_palindrome_message = "'#{phrase2}' is a palindrome"
+    @phrase2_palindrome_message = if phrases.is_palindrome?(phrase2)
+      "'#{phrase2}' is a palindrome"
     else
-      @phrase2_palindrome_message = "'#{phrase2}' is not a palindrome"
+      "'#{phrase2}' is not a palindrome"
     end
 
-    if phrases.is_anagram?(phrase1, phrase2)
-      @anagram_message = "'#{phrase1}' is an anagram of '#{phrase2}'"
+    @anagram_message =  if phrases.is_anagram?(phrase1, phrase2)
+      "'#{phrase1}' is an anagram of '#{phrase2}'"
     else
-      @anagram_message = "'#{phrase1}' is not an anagram of '#{phrase2}'"
+      "'#{phrase1}' is not an anagram of '#{phrase2}'"
     end
 
-    if phrases.is_antigram?(phrase1, phrase2)
-      @antigram_message = "'#{phrase1}' is an antigram of '#{phrase2}'"
+    @antigram_message = if phrases.is_antigram?(phrase1, phrase2)
+      "'#{phrase1}' is an antigram of '#{phrase2}'"
     else
-      @antigram_message = "'#{phrase1}' is not an antigram of '#{phrase2}'"
+      "'#{phrase1}' is not an antigram of '#{phrase2}'"
     end
 
   elsif phrases.is_word?(phrase1) && !phrases.is_word?(phrase2)
 
     @phrase2_palindrome_message = "'#{phrase2}' is not valid input ;("
 
-    if phrases.is_palindrome?(phrase1)
-      @phrase1_palindrome_message = "'#{phrase1}' is a palindrome"
+    @phrase1_palindrome_message = if phrases.is_palindrome?(phrase1)
+      "'#{phrase1}' is a palindrome"
     else
-      @phrase1_palindrome_message = "'#{phrase1}' is not a palindrome"
+      "'#{phrase1}' is not a palindrome"
     end
 
   elsif !phrases.is_word?(phrase1) && phrases.is_word?(phrase2)
 
     @phrase1_palindrome_message = "'#{phrase1}' is not valid input ;("
 
-    if phrases.is_palindrome?(phrase2)
-      @phrase2_palindrome_message = "'#{phrase2}' is a palindrome"
+    @phrase2_palindrome_message = if phrases.is_palindrome?(phrase2)
+      "'#{phrase2}' is a palindrome"
     else
-      @phrase2_palindrome_message = "'#{phrase2}' is not a palindrome"
+      "'#{phrase2}' is not a palindrome"
     end
 
   else
