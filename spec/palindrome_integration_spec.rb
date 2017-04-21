339505ca0c4448b3e7ad24f96be7980e9758c65e
Spencer Alan <spencer.alan@icloud.com>
2017-04-21 16:40:24 -0700

    add integration tests

--- spec/palindrome_integration_spec.rb
+++ spec/palindrome_integration_spec.rb
@@ -5,11 +5,27 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe("the phrase parser path", {:type => :feature}) do
-  it("processes the user input and returns if its a palindrome") do
+  it("processes the user input and returns correct message if its a palindrome") do
     visit("/")
     fill_in("phrase1", :with => "madam")
     fill_in("phrase2", :with => "anagram")
     click_button("what am i?")
     expect(page).to have_content("'madam' is a palindrome")
   end
+  it("processes the user input and returns correct message if input is invalid") do
+    visit("/")
+    fill_in("phrase1", :with => "madam")
+    click_button("what am i?")
+    expect(page).to have_content("'' is not valid input ;(")
+  end
+  it("processes the user input and returns correct message if its a anagram") do
+    visit("/")
+    fill_in("phrase1", :with => "failed")
+    fill_in("phrase2", :with => "a field")
+    click_button("what am i?")
+    expect(page).to have_content("failed' is an anagram of 'a field")
+  end
+
+
+
 end
